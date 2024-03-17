9d5f78f035245d6a9f422f9bd9802b771a50d9e3
MarioMatsas <114489498+MarioMatsas@users.noreply.github.com>
2024-03-17 20:12:43 +0200

    Add files via upload

---
+++ sql_pythoncode.py
@@ -0,0 +1,39 @@
+import csv
+import ast
+
+file = open("keywords.csv", 'r', encoding='utf-8') # encoding='utf-8' is used to read the file in the correct format (with special characters)
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+
+keyword_data = []
+hashkeyword_data = []
+for row in csvreader:
+    movie_id = row[0]
+    data = ast.literal_eval(row[1])
+    for d in data:
+        keyword_data.append(("id", d['id'], "name", d['name']))
+        hashkeyword_data.append(("movie_id", movie_id, "id", d['id']))
+
+#keyword_data = set(keyword_data)
+print(len(keyword_data))
+print(len(set(keyword_data)))
+print(len(hashkeyword_data))
+#print(len(set(hashkeyword_data)))
+file.close()
+
+f = open("Keyword.csv", 'w',newline='', encoding='utf-8')
+writer = csv.writer(f)
+writer.writerow(["id", "name"])
+# Write the data for the keyword table
+for row in keyword_data:
+    writer.writerow([row[1], row[3]])
+f.close()
+
+f = open("hashkeyword.csv", 'w', newline='',encoding='utf-8')
+writer = csv.writer(f)
+writer.writerow(["movie_id", "keyword_id"])
+for row in hashkeyword_data:
+    writer.writerow([row[1], row[3]])
+# Write the data for the hashkeyword table
+f.close()

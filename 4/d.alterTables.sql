ee83f5d3be456adf11d96617440e618f1a22bec0
MarioMatsas <114489498+MarioMatsas@users.noreply.github.com>
2024-04-18 19:12:58 +0300

    Update d.alterTables.sql

--- 4/d.alterTables.sql
+++ 4/d.alterTables.sql
@@ -1,32 +1,20 @@
 -- hasGenre
 ALTER TABLE hasGenre
-ADD CONSTRAINT FK_movieid_hasgenre FOREIGN KEY (movie_id) REFERENCES movie(id);
-
-ALTER TABLE hasGenre
-ADD CONSTRAINT FK_genreid_hasgenre FOREIGN KEY (genre_id) REFERENCES genre(id);
+ADD CONSTRAINT hasgenre_pk PRIMARY KEY (movie_id, genre_id);
 
 -- hasKeyword
 ALTER TABLE haskeyword
-ADD CONSTRAINT FK_movieid_hashkeyword FOREIGN KEY (movie_id) REFERENCES movie(id);
-
-ALTER TABLE haskeyword
-ADD CONSTRAINT FK_keywordid_hashkeyword FOREIGN KEY (keyword_id) REFERENCES Keyword(id);
+ADD CONSTRAINT hashkeyword_pk PRIMARY KEY (movie_id, keyword_id);
 
 -- belongsTocollection
 ALTER TABLE belongsTocollection
-ADD CONSTRAINT FK_movieid_belongsTocollection FOREIGN KEY (movie_id) REFERENCES movie(id);
-
-ALTER TABLE belongsTocollection
-ADD CONSTRAINT FK_collectionid_belongsTocollection FOREIGN KEY (collection_id) REFERENCES collection(id);
-
+ADD CONSTRAINT belongsTocollection_pk PRIMARY KEY (movie_id, collection_id);
 
 -- hasProductioncompany
 ALTER TABLE hasProductioncompany
-ADD CONSTRAINT FK_movieid_hasProductioncompany FOREIGN KEY (movie_id) REFERENCES movie(id);
-
-ALTER TABLE hasProductioncompany
-ADD CONSTRAINT FK_pcid_hasProductioncompany FOREIGN KEY (pc_id) REFERENCES productioncompany(id);
+ADD CONSTRAINT hasProductioncompany_pk PRIMARY KEY (movie_id, pc_id);
 
 -- ratings
 ALTER TABLE ratings
-ADD CONSTRAINT FK_movieid FOREIGN KEY (movie_id) REFERENCES movie(id);
+ADD CONSTRAINT ratings_pk PRIMARY KEY (movie_id, user_id);
+

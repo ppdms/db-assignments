90fa7588d7f5b72f3ee4e38896bcc5c2d6c35d96
MarioMatsas <114489498+MarioMatsas@users.noreply.github.com>
2024-05-10 21:28:47 +0300

    Add files via upload

---
+++ 6/ex.sql
@@ -0,0 +1,58 @@
+/* 1 */
+SELECT year(release_date) as year, COUNT(id) as movies_per_year
+FROM movie
+WHERE budget > 1000000
+GROUP BY year(release_date);
+
+/* 2 */
+SELECT genre_id as genre, COUNT(id) as movies_per_genre
+FROM movie JOIN hasGenre ON id = movie_id
+WHERE budget > 1000000 OR runtime > 120
+GROUP BY genre_id;
+
+/* 3 */
+SELECT genre_id as genre, year(release_date) as year, COUNT(id) as movies_per_gy
+FROM movie JOIN hasGenre ON id = movie_id
+GROUP BY genre_id, year(release_date);
+
+/* 4 */
+SELECT year(release_date) as year, SUM(revenue) as revenues_per_year
+FROM movie JOIN movie_cast ON id = movie_id
+WHERE name = 'Madonna'
+GROUP BY year(release_date);
+
+/* 5 */
+SELECT year(release_date) as year, MAX(budget) as max_budget
+FROM movie
+WHERE budget > 0
+GROUP BY year(release_date);
+
+/* 6 */
+SELECT name as trilogy_name
+FROM collection JOIN belongsTocollection ON id = collection_id
+GROUP BY name
+HAVING COUNT(movie_id) = 3; 
+
+/* 7 */
+SELECT AVG(rating) as avg_rating, COUNT(rating) as rating_count
+FROM ratings
+GROUP BY user_id;
+
+/* 8 */
+SELECT TOP(10) title as movie_title, budget
+FROM movie
+ORDER BY budget DESC;
+
+/* 9 */
+SELECT year(release_date) as year, title as movies_with_max_budget, budget
+FROM movie JOIN (
+    SELECT year(release_date) as year, MAX(budget) as max_budget
+    FROM movie
+    WHERE budget > 0
+    GROUP BY year(release_date)
+) as p1 ON year(movie.release_date) = p1.year AND movie.budget = p1.max_budget
+ORDER BY year(release_date), title;
+
+/* 10 */
+/* 11 */
+/* 12 */
